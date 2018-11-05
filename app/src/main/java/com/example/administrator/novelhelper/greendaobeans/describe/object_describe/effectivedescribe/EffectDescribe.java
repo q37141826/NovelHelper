c71c487626f7177c0cd861493a35bf851182251d
q37141826 <272670440@qq.com>
2018-11-05 18:33:44 +0800

    tianjia le xiaoshuo

---
+++ app/src/main/java/com/example/administrator/novelhelper/greendaobeans/describe/object_describe/effectivedescribe/EffectDescribe.java
@@ -0,0 +1,100 @@
+package com.example.administrator.novelhelper.greendaobeans.describe.object_describe.effectivedescribe;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class EffectDescribe implements Parcelable {
+    private String id;
+    private String object_name;
+    private String skill_name;
+    private String level;
+    private String time;
+    private String describe;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getObject_name() {
+        return object_name;
+    }
+
+    public void setObject_name(String object_name) {
+        this.object_name = object_name;
+    }
+
+    public String getSkill_name() {
+        return skill_name;
+    }
+
+    public void setSkill_name(String skill_name) {
+        this.skill_name = skill_name;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDescribe() {
+        return describe;
+    }
+
+    public void setDescribe(String describe) {
+        this.describe = describe;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.id);
+        dest.writeString(this.object_name);
+        dest.writeString(this.skill_name);
+        dest.writeString(this.level);
+        dest.writeString(this.time);
+        dest.writeString(this.describe);
+    }
+
+    public EffectDescribe() {
+    }
+
+    protected EffectDescribe(Parcel in) {
+        this.id = in.readString();
+        this.object_name = in.readString();
+        this.skill_name = in.readString();
+        this.level = in.readString();
+        this.time = in.readString();
+        this.describe = in.readString();
+    }
+
+    public static final Parcelable.Creator<EffectDescribe> CREATOR = new Parcelable.Creator<EffectDescribe>() {
+        @Override
+        public EffectDescribe createFromParcel(Parcel source) {
+            return new EffectDescribe(source);
+        }
+
+        @Override
+        public EffectDescribe[] newArray(int size) {
+            return new EffectDescribe[size];
+        }
+    };
+}
